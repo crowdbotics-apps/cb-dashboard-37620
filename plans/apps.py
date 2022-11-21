0a1d39eb7ba50870f998425db3472f31d6b3313c
raooll <rahul.aug.1986@gmail.com>
2022-11-21 10:45:16 +0530

    temporary workaround to create plans on startup

--- plans/apps.py
+++ plans/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+from django.core.management import call_command
 
 class PlansConfig(AppConfig):
     name = 'plans'
+
+    def ready(self):
+        # Temporary workaround to create plans
+        call_command('createinitialplans')
